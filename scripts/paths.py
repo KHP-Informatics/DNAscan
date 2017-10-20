2c22ca2fdaab642dc3213eda85571ee31893a38c
AlfredoKCL <alfredo.iacoangeli@gmail.com>
2017-10-20 18:07:01 +0100

    Update paths.py

--- scripts/paths.py
+++ scripts/paths.py
@@ -1,37 +1,59 @@
 #!/home/aga/ENV/bin/python
+
 import os.path
 
+path_iobio = "iobio/"
 
-path_hisat="/users/k1513213/brc_scratch/virus_scan_analysis/"
+path_hisat = "/users/k1513213/brc_scratch/annotation_unility_project/hisat2-2.1.0/"
 
-path_samtools=""
+path_samtools = ""
 
-path_freebayes=""
+path_rtg = ""
 
-path_annovar="~/brc_scratch/local/annovar/"
+path_multiqc = ""
 
-path_annovar_db="~/brc_scratch/local/annovar/humandb/"
+path_fastqc = ""
 
-path_expansionHunter=""
+path_freebayes = "/opt/apps/bioinformatics/freeBayes/v1.0.2-33-gdbb6160-dirty/bin/"
 
-path_expansionHunter_jsons="/users/k1513213/brc_scratch/annotation_unility_project/json/"
+path_annovar = "~/brc_scratch/local/annovar/"
 
-num_cpu="4"
+path_annovar_db = "~/brc_scratch/local/annovar/humandb/"
 
-path_bed="/users/k1513213/brc_scratch/indels_project/gene_list_positions_sorted_no_overlap.bed"
+path_expansionHunter = ""
 
-path_reference="/mnt/lustre/datasets/projectmine/reference/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa"
+path_expansionHunter_jsons = "/users/k1513213/brc_scratch/annotation_unility_project/json_2/"
 
-path_hisat_index="genome"
+port_num = "8080"
 
-path_manta="/users/k1513213/brc_scratch/manta/manta-1.1.1.centos5_x86_64/bin"
+path_java = ""
 
-path_bedtools=""
+path_vcftools = ""
 
-path_tabix=""
+path_gatk = "/opt/apps/bioinformatics/gatk/3.5/"
 
-path_virus_index=""
+num_cpu = "8"
 
-path_virus_bacteria_index=""
+path_bed = "/users/k1513213/brc_scratch/annotation_unility_project/WGS_Miseq_test/miseq/miseq_nochr.bed"
 
+#path_bed="/users/k1513213/brc_scratch/indels_project/gene_list_positions_sorted_no_overlap.bed"
 
+path_reference = "/mnt/lustre/datasets/projectmine/reference/Homo_sapiens/reference_data/Homo_sapiens/GRCh37-lite/GRCh37-lite.fa"
+
+path_hisat_index = "/mnt/lustre/datasets/projectmine/reference/Homo_sapiens/reference_data/Homo_sapiens/GRCh37-lite/GRCh37-lite"
+
+path_manta = "/users/k1513213/brc_scratch/manta/manta-1.1.1.centos5_x86_64/bin/"
+
+path_bedtools = ""
+
+path_tabix = ""
+
+path_virus_index = "/users/k1513213/brc_scratch/annotation_unility_project/hisat2-2.1.0/index/viral.genome"
+
+path_virus_bacteria_index = ""
+
+path_bwa = ""
+
+path_bcftools = ""
+
+path_bwa_index = "/mnt/lustre/datasets/projectmine/reference/Homo_sapiens/reference_data/Homo_sapiens/GRCh37-lite/GRCh37-lite.fa"
