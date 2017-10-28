5167ef9518cb081023f5cde00ac3de4432a029ca
AlfredoKCL <alfredo.iacoangeli@gmail.com>
2017-10-28 18:47:48 +0100

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -181,6 +181,6 @@ sed "s|path_annovar_db = \"\"|path_annovar_db = \"$INSTALL_DIR\/humandb\/\"|" sc
 mv scripts/paths.py_temp  scripts/paths.py
 
 echo "###########################################IMPORTANT######################################################"
-echo "Hisat2-build and bwa-index are still creating their indexes. Please wait untill they complete their task."
+echo "Hisat2-build and bwa-index are still creating their indexes and Annovar is still downloading the CADD database (wget). Please wait untill they complete their task."
 echo "You can check whether or not they are still running using the 'top' command"
 echo "##########################################################################################################"
